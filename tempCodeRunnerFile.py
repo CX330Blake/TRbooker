c5a37e2f6a13bb99b42718bf418a3b8514251620
DESKTOP-COR4OEI\陳子雋 <timchen975@gmail.com>
2024-01-12 16:44:16 +0800

    2024/01/12

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -14,20 +14,16 @@ url = "https://tip.railway.gov.tw/tra-tip-web/tip/tip001/tip112/gobytime"
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
 }
-# 使用者輸入資訊
+# driver_path = "D:/SourceTree/TRbooker/chromedriver.exe"
 departure = input("請輸入出發站: ")
 arrival = input("請輸入抵達站: ")
 date = input("請輸入日期(yyyy/mm/dd): ")
 departure_time = input("24小時制出發時間(hh:mm)(enter跳過): ")
 arrival_time = input("24小時制抵達時間(hh:mm)(enter跳過): ")
 
-#設定web driver
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--log-level=3")
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options=)
+driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(url)
 
-#查找元素，並送出request
 start_station_input = driver.find_element("id", "startStation")
 start_station_input.send_keys(departure)  # 替换为实际的出发站名称
 
@@ -47,41 +43,19 @@ element = WebDriverWait(driver, 10).until(
 response = driver.page_source
 soup = BeautifulSoup(response, "html.parser")
 table = soup.find("table")
-if table:
-    pt_table = pt.PrettyTable()
-    pt_table.field_names = [
-        "車種車次",
-        "出發時間",
-        "抵達時間",
-        "旅程時間",
-        "經由",
-        "全票票價",
-        "優待票票價",
-    ]
 
-    trains = table.find_all("tr", class_="trip-column")
-    for train in trains[1:]:
-        train_data = []
-        train = train.find_all("td")
-        train.pop(9)
-        train.pop(8)
-        train.pop(5)
-        count = 0
-        while count < len(train):
-            train_data.append(train[count].text.replace("\n", ""))
-            count += 1
-        pt_table.add_row(train_data)
-    print(pt_table)
-else:
-    print("未找到")
+pt_table = pt.PrettyTable()
+pt_table.field_names = [
+    "出發時間",
+    "抵達時間",
+    "旅程時間",
+    "車種車次",
+    "經由",
+    "服務設施",
+    "狀態",
+    "全票票價",
+    "優待票票價",
+]
 
-# data = {
-#     "startStation": departure,  # 替换为实际的出发站
-#     "endStation": arrival,  # 替换为实际的抵达站
-#     "rideDate": date,  # 替换为实际的日期
-#     "startTime": departure_time,  # 替换为实际的出发时间
-#     "endTime": arrival_time,  # 替换为实际的抵达时间
-#     "transfer": "",  # 替换为实际的转乘条件，比如 'NORMAL'
-#     "trainTypeList": "",  # 替换为实际的车种条件，比如 'ALL'
-#     "queryClassification": "",  # 替换为实际的查询方式，比如 'NORMAL'
-# }
+train = table.find_all("tr")
+print(train)
